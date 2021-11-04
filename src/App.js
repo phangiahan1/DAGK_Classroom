079c076cd7a0983be2c6ce1e08195e5a71e076aa
phangiahan1 <phanhan226@gmail.com>
2021-11-04 18:36:57 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useState, useEffect } from "react";
+import { Drawer, MyClass } from './components';
+
+function App() {
+  const [createdClasses, setCreatedClasses] = useState([]);
+  const fetchItems = async () => {
+    const data = await fetch('//localhost:4000/classroom');
+    const items = await data.json();
+    console.log(items);
+    setCreatedClasses(items);
+  };
+  // useEffect(() => {
+  //   fetchItems();
+  // },[]
+  // );
+  useEffect(() => {
+    fetchItems();
+  }
+  );
+  return (
+    <div className="App">
+      <Drawer />
+      <ol className="joined">
+        {createdClasses.map((item) => (
+          <MyClass classData={item} />
+        ))}
+      </ol>
+    </div>
+  );
+}
+
+export default App;
