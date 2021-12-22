a296af69803adb27e6ae2ce6cbb178da24c7a953
phangiahan1 <phanhan226@gmail.com>
2021-12-22 16:42:33 +0700

    api change

--- src/context/constants.js
+++ src/context/constants.js
@@ -1,6 +1,6 @@
 export const apiUrl =
 	process.env.NODE_ENV !== 'production'
 		? 'http://localhost:5000'
-		: 'https://thclassroom-api-app.herokuapp.com'
+		: 'https://btn2-web-grades.herokuapp.com'
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'tokenData'
